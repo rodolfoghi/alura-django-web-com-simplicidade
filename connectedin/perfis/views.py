96bb8c40a215b7eb63c9eb622413b9ce1784a8e3
rodolfoghi <rodolfoghi@gmail.com>
2016-05-16 20:35:35 -0300

    Capítulo 4 (Primeiros passos com persistência)

--- connectedin/perfis/views.py
+++ connectedin/perfis/views.py
@@ -4,15 +4,6 @@ from perfis.models import Perfil
 def index(request):
 	return render(request, 'index.html')
 
-def exibir(request, perfil_id):
-	
-	perfil = Perfil()
-
-	if perfil_id == '1':
-		perfil = Perfil('Carlos Eduardo', 'carlos.eduardo@gmail.com', '99999', 'Google Inc')
-
-	if perfil_id == '2':
-		perfil = Perfil('Hermes', 'hermes@gmail.com', '8888', 'Apple')
-
-
+def exibir(request, perfil_id):	
+	perfil = Perfil.objects.get(id=perfil_id)
 	return render(request, 'perfil.html', {"perfil" : perfil })
