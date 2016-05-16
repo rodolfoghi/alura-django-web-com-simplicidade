99450ffdede154eab0bec78cd3f5e510849c2639
rodolfoghi <rodolfoghi@gmail.com>
2016-05-16 19:56:35 -0300

    Capítulo 3 (A página Perfil)

--- connectedin/perfis/urls.py
+++ connectedin/perfis/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^', include('perfis.urls'))
+    url(r'^$', 'perfis.views.index'),
+    url(r'^perfis/(?P<perfil_id>\d+)$', 'perfis.views.exibir')
 )
--- connectedin/perfis/views.py
+++ connectedin/perfis/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from perfis.models import Perfil
 
-# Create your views here.
+def index(request):
+	return render(request, 'index.html')
+
+def exibir(request, perfil_id):
+	
+	perfil = Perfil()
+
+	if perfil_id == '1':
+		perfil = Perfil('Carlos Eduardo', 'carlos.eduardo@gmail.com', '99999', 'Google Inc')
+
+	if perfil_id == '2':
+		perfil = Perfil('Hermes', 'hermes@gmail.com', '8888', 'Apple')
+
+
+	return render(request, 'perfil.html', {"perfil" : perfil })
