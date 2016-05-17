8400ee21307f527f4436cb21f690aa45ff43d2b7
rodolfoghi <rodolfoghi@gmail.com>
2016-05-16 21:12:48 -0300

    Capítulo 5 (Selecionando perfis de uma lista)

--- connectedin/perfis/views.py
+++ connectedin/perfis/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from perfis.models import Perfil
 
-def index(request):
-	return render(request, 'index.html')
+def index(request):	
+	return render(request, 'index.html', { "perfis" : Perfil.objects.all()})
 
 def exibir(request, perfil_id):	
 	perfil = Perfil.objects.get(id=perfil_id)
