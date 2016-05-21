f01cfc1b665e611d8726a5a7993b6ce05a2964fb
rodolfoghi <rodolfoghi@gmail.com>
2016-05-21 07:07:52 -0300

    Capítulo 6 (convidado perfis)

--- connectedin/perfis/urls.py
+++ connectedin/perfis/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from perfis import views
 
 urlpatterns = patterns('',
-    url(r'^$', 'perfis.views.index', name='index'),
-    url(r'^perfis/(?P<perfil_id>\d+)$', 'perfis.views.exibir', name='exibir')
+    url(r'^$', views.index, name='index'),
+    url(r'^perfis/(?P<perfil_id>\d+)$', views.exibir, name='exibir'),
+    url(r'^perfis/(?P<perfil_id>\d+)/convidar$', views.convidar, name='convidar')
 )
